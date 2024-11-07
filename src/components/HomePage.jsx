e49f0afafd51084a4a681d5ed0ff5acff578269b
wisdom oladipupo <wisbel71@gmail.com>
2024-11-07 20:15:33 +0100

    first commit

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const HomePage = () => {
+  return (
+    <div>
+        <h1>pic</h1>
+        <img src="#" alt="" />
+    </div>
+  )
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+const Nav = () => {
+  return (
+    <div>
+      <div>
+        <h1>Plan Life</h1>
+        <p>Healthy Meal, Healthy Life</p>
+        <button>Create New</button>
+      </div>
+      <p>Hello Brian</p>
+      <h2>Discover Recipes</h2>
+      <input
+        type="text"
+        placeholder="Search for Recipes,Ingredients and Tags"
+      />
+    </div>
+  );
+};
+
+export default Nav;
